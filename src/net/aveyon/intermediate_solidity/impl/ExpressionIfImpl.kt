67af04165ff09378bccf750173538182bd65b768
Andreas Schimanowski <ansch091@stud.fh-dortmund.de>
2022-05-28 22:38:33 +0200

    Added copy ctor for if and string expressions

--- src/net/aveyon/intermediate_solidity/impl/ExpressionIfImpl.kt
+++ src/net/aveyon/intermediate_solidity/impl/ExpressionIfImpl.kt
@@ -1,12 +1,33 @@
 package net.aveyon.intermediate_solidity.impl
 
+import net.aveyon.intermediate_solidity.Expression
 import net.aveyon.intermediate_solidity.ExpressionIf
+import net.aveyon.intermediate_solidity.ExpressionString
+import java.util.LinkedList
 
 class ExpressionIfImpl(): ExpressionIf() {
 
     /**
      * Copy Ctor
-     * TODO
      */
-    constructor(copySource: ExpressionIf): this() {}
+    constructor(copySource: ExpressionIf): this() {
+        copySource.conditions.forEach {
+            val expressions: MutableList<Expression> = LinkedList()
+            it.second.forEach { exp ->
+                when (exp) {
+                    is ExpressionString -> {
+                        expressions.add(ExpressionStringImpl(exp))
+                    }
+                    is ExpressionIf -> { // Expression If
+                        expressions.add(ExpressionIfImpl(exp))
+                    }
+                    else -> {
+                        // err
+                    }
+                }
+            }
+
+            conditions.add(net.aveyon.intermediate_solidity.util.Pair(it.first, expressions))
+        }
+    }
 }
--- src/net/aveyon/intermediate_solidity/impl/ExpressionStringImpl.kt
+++ src/net/aveyon/intermediate_solidity/impl/ExpressionStringImpl.kt
@@ -3,4 +3,9 @@ package net.aveyon.intermediate_solidity.impl
 import net.aveyon.intermediate_solidity.ExpressionString
 
 class ExpressionStringImpl(value: String): ExpressionString(value) {
+
+    /**
+     * Copy Ctor
+     */
+    constructor(copySource: ExpressionString): this(copySource.value)
 }
