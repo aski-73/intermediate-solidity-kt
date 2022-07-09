20bc3e569e8e5bc1a0d78d0582fd0edbe2bfd8a5
Andreas Schimanowski <ansch091@stud.fh-dortmund.de>
2022-07-09 22:15:30 +0200

    Constructor not payable anymore because no need

--- src/net/aveyon/intermediate_solidity/Constructor.kt
+++ src/net/aveyon/intermediate_solidity/Constructor.kt
@@ -5,6 +5,8 @@ import java.util.LinkedList
 abstract class Constructor: Node("ctor") {
     val expressions: MutableList<Expression> = LinkedList()
     val modifiers: MutableList<Modifier> = LinkedList()
-    var payable: Boolean = true
+    // Does not need payable since receive() and fallback() functions handle ether transfers.
+    var payable: Boolean = false
+    // ctors don't have a visibility. Just set it to public.
     var visibility: Visibility = Visibility.PUBLIC
 }
